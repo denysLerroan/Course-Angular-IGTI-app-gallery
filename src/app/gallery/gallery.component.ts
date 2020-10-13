2ef8725674a1aa0e9fe6057433a7013dc8c9aaea
denysLerroan <lerroandeco@gmail.com>
2020-10-12 22:37:12 -0300

    previous() & next() function

---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { kMaxLength } from 'buffer';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css'],
+})
+export class GalleryComponent {
+  @Input() title: string = '';
+  @Input() photos: string[] = [];
+
+  currentImg: number = 0;
+
+  next() {
+    this.currentImg++;
+  }
+  previous() {
+    this.currentImg--;
+  }
+  firstImg() {
+    this.currentImg = 0;
+  }
+
+  finalImg() {
+    this.currentImg = this.photos.length;
+  }
+}
